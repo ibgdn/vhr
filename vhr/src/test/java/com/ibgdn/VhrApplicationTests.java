ac9fd6573b5a64f994612f4104b62460f94f187f
ibgdn <ibgdn4@gmail.com>
2020-06-13 18:25:51 +0800

    后台项目初始化提交，排除 maven 配置文件。

---
+++ vhr/src/test/java/com/ibgdn/VhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibgdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
